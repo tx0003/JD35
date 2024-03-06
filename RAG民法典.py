4b09edfd98e0aa3d1817b4c0b4759a97f0e68bdc
tx0003 <160549561+tx0003@users.noreply.github.com>
2024-03-06 21:13:41 +0000

    MFD

---
+++ RAG民法典.py
@@ -0,0 +1,62 @@
+import os
+from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+
+os.environ["OPENAI_API_KEY"]="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"
+ 
+chat=ChatOpenAI(
+     openai_api_key=os.environ["OPENAI_API_KEY"],
+     model='gpt-3.5-turbo'
+ )
+
+#载入民法典pdf
+loader=PyPDFLoader("民法典.pdf")
+pages=loader.load_and_split()
+
+#分割文本
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter=RecursiveCharacterTextSplitter(
+    chunk_size=100,
+    chunk_overlap=20,
+)
+docs=text_splitter.split_documents(pages)
+
+#利用embedding 模型对每一个文本片段进行向量化，并储存到向量数据库中
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+
+embed_model=OpenAIEmbeddings()
+vectorstore=Chroma.from_documents(documents=docs,embedding=embed_model,collection_name="openai_embed") 
+
+#增强
+def augment_prompt(query:str):
+    results=vectorstore.similarity_search(query,k=10)
+    source_knowledge="\n".join([x.page_content for x in results])
+    augmented_prompt= f"""Using the contexts below, answer the query.
+    
+    contexts:
+    {source_knowledge}
+    
+    query:{query}"""
+    return augmented_prompt
+    
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+messages=[
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="knock knock."),
+    AIMessage(content="Who is there"),
+    HumanMessage(content="Orange"),
+]
+
+query=input("请提出你的问题：")
+
+prompt=HumanMessage(
+    content=augment_prompt(query)
+)
+messages.append(prompt)
+res=chat(messages)
+print(res.content)
